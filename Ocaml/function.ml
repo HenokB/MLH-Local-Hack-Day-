049880c98b8564564fa3b31afcc5e387a3362d35
Henok Biadglign Ademtew <46082799+HenokB@users.noreply.github.com>
2021-10-14 09:34:52 -0400

    Create function.ml

---
+++ Ocaml/function.ml
@@ -0,0 +1,4 @@
+# let square_function x = x * x;;
+val square_function : int -> int = <fun>
+# square_function 50;; (**here we are passing 50 to the function. That means it will multiply 50 by 50 and give us 2500**)
+- : int = 2500
