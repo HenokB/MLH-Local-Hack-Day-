5a0493a41385cf93b7800f74ad26175e55290f9d
Henok Biadglign Ademtew <46082799+HenokB@users.noreply.github.com>
2021-10-14 09:15:52 -0400

    Create datatypes.ml

---
+++ Ocaml/datatypes.ml
@@ -0,0 +1,17 @@
+(** Primitive Data Types **)
+
+(**Integers**)
+let x: int = 0
+
+(**Float**)
+let x: float = 0.0
+
+(**Boolean**)
+let x: bool = true
+
+(**Character**)
+let x: char = 'm'
+
+(**String**)
+let x: string = "local hack day"
+
